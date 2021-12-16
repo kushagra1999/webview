83125519de9f9837026858770dbe2bb153b1354e
kushagra1999 <mishraa.kushagra@gmail.com>
2021-12-16 14:04:54 +0530

    PASSING HTML EXTERNALLY

--- webview/ViewController.swift
+++ webview/ViewController.swift
@@ -31,10 +31,35 @@ class ViewController: UIViewController, WKNavigationDelegate{
 
         let script = WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
         contentController.addUserScript(script)
-        if let indexURL = Bundle.main.url(forResource: "index",
-                                          withExtension: "html") {
-            webView.loadFileURL(indexURL, allowingReadAccessTo: indexURL.deletingLastPathComponent())
-        }
+//        if let indexURL = Bundle.main.url(forResource: "index",
+//                                          withExtension: "html") {
+//            webView.loadFileURL(indexURL, allowingReadAccessTo: indexURL.deletingLastPathComponent())
+//        }
+        let htmlString = """
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <title>ExploreWKWebViewJavascript</title>
+    </head>
+    <body>
+        Type something:
+    <input type = "text"> <br>
+        <input type="submit" value="Geocode Address" onclick="geocodeAddress();">
+            
+<!--            <form action="https://ptsv2.com/t/xzmpy-1639581165/post">-->
+<form>
+              <label for="fname">First name:</label>
+              <input type="text" id="fname" name="fname"><br><br>
+              <label for="lname">Last name:</label>
+              <input type="text" id="lname" name="lname"><br><br>
+              <input type="submit" value="Submit">
+            </form>
+    </body>
+    </html>
+
+"""
+        webView.loadHTMLString(htmlString, baseURL: nil)
         
     }
 
