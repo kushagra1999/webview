af08424e423f7ae03b68ae90996e5aa18512a22e
kushagra1999 <mishraa.kushagra@gmail.com>
2021-12-16 15:43:17 +0530

    html payload

--- webjs/NotificationViewController.swift
+++ webjs/NotificationViewController.swift
@@ -13,6 +13,7 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
 
     @IBOutlet var label: UILabel?
     var webView: WKWebView!
+    var myhtml = ""
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
@@ -21,26 +22,36 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any required interface initialization here.
+        print("Entered view didload")
         let contentController = self.webView.configuration.userContentController
         contentController.add(self, name: "buttonMessage")
         let js = """
          function geocodeAddress() {
           
-                 webkit.messageHandlers.buttonMessage.postMessage("Data sent");
-                 
+                                webkit.messageHandlers.buttonMessage.postMessage("Data sent2");
+              
              
          }
         """
 
         let script = WKUserScript(source: js, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
         contentController.addUserScript(script)
-        if let indexURL = Bundle.main.url(forResource: "index",
-                                          withExtension: "html") {
-            webView.loadFileURL(indexURL, allowingReadAccessTo: indexURL.deletingLastPathComponent())
-        }
+        
+//        if let indexURL = Bundle.main.url(forResource: "index",
+//                                          withExtension: "html") {
+//            webView.loadFileURL(indexURL, allowingReadAccessTo: indexURL.deletingLastPathComponent())
+//        }
     }
-    
+    func getCustomdata(_ notification: UNNotification){
+        if let dict = notification.request.content.userInfo as? [String:Any] {
+                  if let dataDict = dict["Timer_Customdata"] as? [String:Any],let type = dataDict["myhtml"] as? String{
+                      self.myhtml = type
+                  }
+              }
+    }
     func didReceive(_ notification: UNNotification) {
+        getCustomdata(notification)
+        webView.loadHTMLString(myhtml, baseURL: nil)
             self.preferredContentSize = CGSize(width: UIScreen.main.bounds.size.width, height: 1000)
             self.view.setNeedsUpdateConstraints()
             self.view.setNeedsLayout()
